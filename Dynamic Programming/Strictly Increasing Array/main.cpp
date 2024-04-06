ef268e10eb0d6bcb834aed337469d455139c9086
Raghav <raghavsekhri2k1@gmail.com>
2024-04-06 17:47:56 +0530

    dsa

--- Dynamic Programming/Strictly Increasing Array/main.cpp
+++ Dynamic Programming/Strictly Increasing Array/main.cpp
@@ -8,13 +8,13 @@ class Solution {
     int min_operations(vector<int>& nums) {
         int n = nums.size();
         vector<int> dp(n, 1);
+        // lis code
         for (int i = 1; i < n; i++) {
             for (int j = 0; j < i; j++) {
                 if (nums[i] > nums[j] && (i - j) <= (nums[i] - nums[j])) {
                     dp[i] = max(dp[i], dp[j] + 1);
                 }
             }
-            // len = max(len, LIS[i]);
         }
 
         return n - *max_element(dp.begin(), dp.end());
